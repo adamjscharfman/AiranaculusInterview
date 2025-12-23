857bbaf68fc624a0181a2d971554cf9606dd0e75
adamjscharfman <adamjscharfman@gmail.com>
2025-12-23 14:43:45 -0500

    Added function to estimate group delay of filter

--- lib/frequency_response.py
+++ lib/frequency_response.py
@@ -8,3 +8,8 @@ def compute_welch_psd(iq_data:np.array,nperseg:int=1024,noverlap:int=512,nfft:in
 def compute_spectrogram(iq_data:np.array,nperseg:int=1024,noverlap:int=512,nfft:int=1024,fs:float=1.0):
     f, t, Sxx = signal.spectrogram(iq_data,nperseg=nperseg,noverlap=noverlap,nfft=nfft,fs=fs,return_onesided=False,mode='magnitude')
     return np.fft.fftshift(f),t,np.fft.fftshift(Sxx,axes=0)
+
+def compute_group_delay(w:np.array,H:np.array):
+    gd = -np.diff(np.unwrap(np.angle(H))) / np.diff(w)
+    return gd
+
